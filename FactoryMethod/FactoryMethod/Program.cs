232ecd3765f1aa23760451cd1829ec0b8c8fb9c4
“Dmytro <dmytro_ivanchyshyn@ukr.net>
2015-12-18 22:28:53 +0100

    factoryMethod

---
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var creator = new ConcreteCreator();
+            var product = creator.FactoryMethod();
+
+            creator.AnOperation();
+        }
+    }
+}
